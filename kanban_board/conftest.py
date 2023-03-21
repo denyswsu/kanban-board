6317a1e9150341638659123c2d0aac2b19aa9953
dannytarn <denys.tarnavskyi@wesoftyou.com>
2023-03-21 14:44:25 +0200

    skeleton

---
+++ kanban_board/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from kanban_board.users.models import User
+from kanban_board.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
